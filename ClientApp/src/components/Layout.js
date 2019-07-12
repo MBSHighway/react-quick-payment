aae6e436f0a6123fdb95d9e5bcd712daeeb1fe47
Andrew Fellman <andrewf@mbshighway.com>
2019-07-12 11:27:54 -0400

    Initial

---
+++ ClientApp/src/components/Layout.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import { NavMenu } from './NavMenu';
+
+export class Layout extends Component {
+  static displayName = Layout.name;
+
+    componentDidMount = () => {
+        const hubConnection = new HubConnection('http://localhost:5000/chat');
+
+        this.setState({ hubConnection, nick }, () => {
+            this.state.hubConnection
+                .start()
+                .then(() => console.log('Connection started!'))
+                .catch(err => console.log('Error while establishing connection :('));
+        });
+    }
+  render () {
+    return (
+      <div>
+        <NavMenu />
+        <Container>
+          {this.props.children}
+        </Container>
+      </div>
+    );
+  }
+}
